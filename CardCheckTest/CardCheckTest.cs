9b619b28a1457175aa1eb5efaa441ad7486de1ab
SuperGent <paul@gentmail.com>
2014-08-03 14:42:16 +0100

    Added Tests

---
+++ CardCheckTest/CardCheckTest.cs
@@ -0,0 +1,47 @@
+﻿using CreditCard_Check;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CardCheckTest
+{
+    [TestClass]
+    public class CardCheckTest
+    {
+        [TestMethod]
+        public void NumberValid16()
+        {
+            string testNumber = "1234567890123452";
+            bool result = CardCheck.CheckNumber(testNumber);
+
+            Assert.IsTrue(result);
+
+        }
+        [TestMethod]
+        public void NumberValid15()
+        {
+            string testNumber = "123456789012347";
+            bool result = CardCheck.CheckNumber(testNumber);
+
+            Assert.IsTrue(result);
+
+        }
+        [TestMethod]
+        public void NumberValid3()
+        {
+            string testNumber = "125";
+            bool result = CardCheck.CheckNumber(testNumber);
+
+            Assert.IsTrue(result);
+
+        }
+        [TestMethod]
+        public void NumberInvalid16()
+        {
+            string testNumber = "123456789012345";
+            bool result = CardCheck.CheckNumber(testNumber);
+
+            Assert.IsFalse(result);
+
+        }
+    }
+}
